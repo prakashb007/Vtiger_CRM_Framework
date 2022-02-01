30197a65d1e81c94de2f63e909cf118cf78c51a2
rohitsahoo <rohit.sahoo50@gmail.com>
2022-02-01 11:55:44 +0530

    Committed on 01/02/2022 11:56 AM

---
+++ src/main/java/crm/crm/vtiger/genericLibrary/IPathConstants.java
@@ -0,0 +1,10 @@
+package crm.crm.vtiger.genericLibrary;
+/**
+ * This Interface contains all the specified File Paths which will be used in Test Scripts
+ * @author rohit
+ *
+ */
+public interface IPathConstants {
+	String FilePath ="./data/CommonData.properties";
+	String ExcelPath="./data/testscriptdata.xlsx";
+}
